90499dadbcf658bf6b0bcc8b41acffeed416efa9
Selami Hoxha <selami_h55@hotmail.com>
2021-11-19 13:14:48 +0100

    A copy of the Attacks.jar is now created as Bleichenbacher.jar and has the same functionality.

---
+++ Bleichenbacher/src/main/java/de/rub/nds/tlsbreaker/bleichenbacher/bruteforce/GuessProviderType.java
@@ -0,0 +1,25 @@
+/**
+ * TLS-Breaker - A tool collection of various attacks on TLS based on TLS-Attacker
+ *
+ * Copyright 2021-2021 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+
+package de.rub.nds.tlsbreaker.bleichenbacher.bruteforce;
+
+/**
+ * A Type of GuessProvider.
+ */
+public enum GuessProviderType {
+
+    /**
+     * An IncrementingGuessProvider just tries all byte[] sequences in order
+     */
+    INCREMENTING,
+    /**
+     * A WordListGuessProvider uses an InputSource to try all words from the InputSource
+     */
+    WORDLIST
+}
--- Bleichenbacher/src/main/java/de/rub/nds/tlsbreaker/bleichenbacher/pkcs1/oracles/RealDirectMessagePkcs1Oracle.java
+++ Bleichenbacher/src/main/java/de/rub/nds/tlsbreaker/bleichenbacher/pkcs1/oracles/RealDirectMessagePkcs1Oracle.java
@@ -6,16 +6,6 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-/**
- * TLS-Breaker - A tool collection of various attacks on TLS based on
-							TLS-Attacker
- *
- * Copyright 2021-2021 Ruhr University Bochum, Paderborn University, Hackmanit
-							GmbH
- *
- * Licensed under Apache License, Version 2.0
- * http://www.apache.org/licenses/LICENSE-2.0.txt
- */
 
 package de.rub.nds.tlsbreaker.bleichenbacher.pkcs1.oracles;
 
