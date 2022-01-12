7cb12d0a8943ef94a29d61201fa2b5d01a737697
shazib78 <shajibs78@gmail.com>
2022-01-12 15:36:55 +0100

    Copyright year update

--- Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/impl/drown/SpecialDrownAttacker.java
+++ Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/impl/drown/SpecialDrownAttacker.java
@@ -1,12 +1,11 @@
 /**
  * TLS-Breaker - A tool collection of various attacks on TLS based on TLS-Attacker
  *
- * Copyright 2021-2021 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
+ * Copyright 2021-2022 Ruhr University Bochum, Paderborn University, Hackmanit GmbH
  *
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-
 package de.rub.nds.tlsbreaker.attacks.impl.drown;
 
 import static de.rub.nds.tlsattacker.util.ConsoleLogger.CONSOLE;
