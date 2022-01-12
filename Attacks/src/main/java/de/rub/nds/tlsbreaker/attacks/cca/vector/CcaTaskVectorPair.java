4384cde1dd4cb761bbf6fbc56b1bbd4036121f14
shazib78 <shajibs78@gmail.com>
2022-01-12 15:48:10 +0100

    Copyright year update

--- Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/cca/vector/CcaTaskVectorPair.java
+++ Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/cca/vector/CcaTaskVectorPair.java
@@ -6,6 +6,7 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
+
 package de.rub.nds.tlsbreaker.attacks.cca.vector;
 
 import de.rub.nds.tlsbreaker.attacks.task.CcaTask;
--- Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/pkcs1/Pkcs1Vector.java
+++ Attacks/src/main/java/de/rub/nds/tlsbreaker/attacks/pkcs1/Pkcs1Vector.java
@@ -6,6 +6,7 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
+
 package de.rub.nds.tlsbreaker.attacks.pkcs1;
 
 import de.rub.nds.modifiablevariable.util.ArrayConverter;
