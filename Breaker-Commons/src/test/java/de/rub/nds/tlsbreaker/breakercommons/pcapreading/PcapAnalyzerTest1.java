b169364053145f97287a6ca1151fb8b319f256bc
SUBRAMANYA <psubramanyashetty@gmail.com>
2022-01-12 13:53:27 +0100

    Updated testscripts to run on linux

--- Breaker-Commons/src/test/java/de/rub/nds/tlsbreaker/breakercommons/pcapreading/PcapAnalyzerTest1.java
+++ Breaker-Commons/src/test/java/de/rub/nds/tlsbreaker/breakercommons/pcapreading/PcapAnalyzerTest1.java
@@ -12,21 +12,21 @@ package de.rub.nds.tlsbreaker.breakercommons.pcapreading;
 import de.rub.nds.tlsbreaker.breakercommons.util.pcap.PcapAnalyzer;
 import de.rub.nds.tlsbreaker.breakercommons.util.pcap.PcapSession;
 import org.apache.commons.codec.binary.Hex;
+
+import java.io.File;
 import java.util.List;
-import java.util.Arrays;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class PcapAnalyzerTest1 {
 
-    private String FileLocation, PmsStoredValues;
+    private String PmsStoredValues;
+    private File FileLocation;
 
     public PcapAnalyzerTest1() {
-        FileLocation = "src\\test\\resources\\pcap_files\\Sample1.pcapng";
+        FileLocation = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator
+            + "pcap_files" + File.separator + "Sample1.pcapng" + File.separator);
         PmsStoredValues =
             "60ca0832ac2eb6130b3d695e4e1308f6241b5cbd7e57d3530ff311ebffd47910d67d7f835a6ce8ad859f51cd0f07e794acd6c133f35f67e9b7e18b3f3c67c793d1bb9fd865d661e32317f3f1e95e480998218b9bb09ff90bc2482c32c6e2e4905545980d35b565fecdfd06b1861fb641d151b823abc11e917c5f73d2daeb7231";
 
@@ -42,7 +42,8 @@ public class PcapAnalyzerTest1 {
 
     @Test
     public void testgetPreMasterSecret() {
-        PcapAnalyzer sample = new PcapAnalyzer(FileLocation);
+
+        PcapAnalyzer sample = new PcapAnalyzer(FileLocation.getPath());
 
         List<PcapSession> sessions = sample.getAllSessions();
         byte[] pms = sample.getPreMasterSecret(sessions.get(0).getClientKeyExchangeMessage());
--- Breaker-Commons/src/test/java/de/rub/nds/tlsbreaker/breakercommons/pcapreading/PcapAnalyzerTest3.java
+++ Breaker-Commons/src/test/java/de/rub/nds/tlsbreaker/breakercommons/pcapreading/PcapAnalyzerTest3.java
@@ -12,25 +12,27 @@ package de.rub.nds.tlsbreaker.breakercommons.pcapreading;
 import de.rub.nds.tlsbreaker.breakercommons.util.pcap.PcapAnalyzer;
 import de.rub.nds.tlsbreaker.breakercommons.util.pcap.PcapSession;
 import org.apache.commons.codec.binary.Hex;
+
+import java.io.File;
 import java.util.List;
 import java.util.Arrays;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class PcapAnalyzerTest3 {
 
-    private String FileLocation;
+    private File FileLocation;
     List<String> PmsStoredValues = Arrays.asList(
         "707eb01ca5981df78cbe2a44f5c667039d2c35d77db180c89857c534c8ad8b775c6427403bd863e50e9218c10c12e4e22b244089bd6fe0ae19806c20f9ba246a346b147e9351ccbe4b93048cef2e1af6f5a27b02fdf8ddd50cd9de6eecee8378bd45de724f06ffa22fea6c0ff53cbc10321f0ade472f7a689735f80ad432ff20",
         "20ebbf46d49c0dc18b4fc26dce50ce5a6c5ce7ecc79fa236546476da91571625d10b5e9e14a5ffdbac37e1bdf8461d13811dfa72ebb65ce1d754c80307ba2fd492435504b341e7cce1ce724e611ef297372dc9fa7ff7178d361f8bcdbe485292d1bb7847756c54c2e28c8780838b9d10d1b9c8f8445411ff539507d3bb271a2a",
         "60514d9424f0c47db8e45a779cd4ef5f9008560199be8266332d1e365e8782f9b0c3bc18d7c28320ebce0fd84226a401f8754cabb0fc6ae4a664d345176f80d06deb9f31e69e0fbacb7c84eed93fc0ff48b3e7e97092028f9e99a79696854f73d0aa43108dcd9d941275274c1417ef52496eebab76075ff443bd3ce44bab1dff");
 
     public PcapAnalyzerTest3() {
-        FileLocation = "src\\test\\resources\\pcap_files\\Sample3.pcap";
+
+        FileLocation = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator
+            + "pcap_files" + File.separator + "Sample3.pcap" + File.separator);
+
     }
 
     @BeforeClass
@@ -43,7 +45,8 @@ public class PcapAnalyzerTest3 {
 
     @Test
     public void testgetPreMasterSecret() {
-        PcapAnalyzer sample = new PcapAnalyzer(FileLocation);
+
+        PcapAnalyzer sample = new PcapAnalyzer(FileLocation.getPath());
 
         List<PcapSession> sessions = sample.getAllSessions();
 
