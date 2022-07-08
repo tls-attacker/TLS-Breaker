62e8772cbc0d1ddb6201852d3184b1b8be7d55bb
imperfectdeveloper <selami_h55@hotmail.com>
2022-07-08 13:55:05 +0200

    Keeps the connection alive

--- SimpleMitmProxy/src/main/java/de/rub/nds/tlsbreaker/simplemitmproxy/impl/SimpleMitmProxy.java
+++ SimpleMitmProxy/src/main/java/de/rub/nds/tlsbreaker/simplemitmproxy/impl/SimpleMitmProxy.java
@@ -11,6 +11,8 @@ package de.rub.nds.tlsbreaker.simplemitmproxy.impl;
 
 import de.rub.nds.tlsbreaker.breakercommons.impl.Attacker;
 import de.rub.nds.tlsbreaker.simplemitmproxy.config.SimpleMitmProxyCommandConfig;
+
+import java.io.IOException;
 import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.constants.RunningModeType;
 import de.rub.nds.tlsattacker.core.state.State;
@@ -18,6 +20,7 @@ import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutor;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowExecutorFactory;
 import de.rub.nds.tlsattacker.core.workflow.action.executor.WorkflowExecutorType;
 import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
+import de.rub.nds.tlsattacker.transport.TransportHandler;
 
 /**
  *
@@ -35,17 +38,35 @@ public class SimpleMitmProxy extends Attacker<SimpleMitmProxyCommandConfig> {
 
     @Override
     public void executeAttack() {
-        Config conf = super.getBaseConfig();
+        Config conf = config.createConfig();
         conf.setWorkflowTraceType(WorkflowTraceType.SIMPLE_MITM_PROXY);
         conf.setDefaultRunningMode(RunningModeType.MITM);
         conf.setWorkflowExecutorShouldClose(false);
 
         State state = new State(conf);
 
-        WorkflowExecutor workflowExecutor =
-            WorkflowExecutorFactory.createWorkflowExecutor(WorkflowExecutorType.DEFAULT, state);
+        WorkflowExecutor workflowExecutor = WorkflowExecutorFactory.createWorkflowExecutor(WorkflowExecutorType.DEFAULT,
+                state);
         workflowExecutor.executeWorkflow();
 
+        TransportHandler handler = state.getInboundTlsContexts().get(0).getTransportHandler();
+        while (true) {
+            try {
+                byte[] the_data = handler.fetchData();
+                if (the_data != null) {
+                    String string_data = new String(the_data);
+                    if(!string_data.isEmpty()){
+                        System.out.println(string_data);
+                        System.out.print(the_data.length);
+                        System.out.println("Data fetched");
+                    }
+                    
+                }
+            } catch (IOException e) {
+                System.out.println("No data yet");
+                e.printStackTrace();
+            }
+        }
     }
 
     /**
