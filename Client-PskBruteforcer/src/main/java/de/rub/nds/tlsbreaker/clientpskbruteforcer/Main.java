8aaa1d1ba17d20095737cfdd5a66c1634e6d6603
SUBRAMANYA <psubramanyashetty@gmail.com>
2022-09-09 12:28:21 +0200

    Code Clean up

--- Client-PskBruteforcer/src/main/java/de/rub/nds/tlsbreaker/clientpskbruteforcer/Main.java
+++ Client-PskBruteforcer/src/main/java/de/rub/nds/tlsbreaker/clientpskbruteforcer/Main.java
@@ -54,6 +54,8 @@ public class Main {
             return;
         }
 
+        pskBruteForcerAttackClientTest.setSkipConnectionCheck(true);
+
         if (pskBruteForcerAttackClientTest.getPcapFileLocation() != null) {
             if (FileUtils.isFileExists(pskBruteForcerAttackClientTest.getPcapFileLocation())) {
                 try {
